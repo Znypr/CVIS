114859cc08810eca9eab8cc7904678909bbc2a63
Znypr <mulyadhic@gmail.com>
2021-10-20 20:56:38 +0200

    new save function

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import cv2
+import imghdr
 
 w, h = 600, 300
 
@@ -6,12 +7,19 @@ def set_window_size(width,height):
     global w, h
     w,h = width, height
 
-def show(image):
+def show(images):
 
-    for i in range(len(image)):
-        cv2.namedWindow(image[i][0], cv2.WINDOW_KEEPRATIO)
-        cv2.imshow(image[i][0], image[i][1])
-        cv2.resizeWindow(image[i][0], w, h)
+    for i in range(len(images)):
+        cv2.namedWindow(images[i][0], cv2.WINDOW_KEEPRATIO)
+        cv2.imshow(images[i][0], images[i][1])
+        cv2.resizeWindow(images[i][0], w, h)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def save(images, type, folder):
+
+
+    for i in range(1, len(images)):
+        path = "img/" + folder +"/"+ folder + "_" + images[i][0] + "." + type
+        cv2.imwrite(path, images[i][1])
