a808681443217b547aadcaaa154c6ebd936484ff
Znypr <mulyadhic@gmail.com>
2021-10-20 20:57:10 +0200

    clean up, usage of functions.save()

--- handling_images.py
+++ handling_images.py
@@ -4,26 +4,22 @@ import functions
 
 if __name__ == '__main__':
 
-
-    # reading
-    img = cv2.imread("img/forest/forest.jpeg", 1)
-
-    # filtering
-    imgB = img[:,:,0]
-    imgG = img[:,:,1]
-    imgR = img[:,:,2]
-
-    # saving
-    cv2.imwrite("img/forest/pic1_blue.jpg", imgB)
-    cv2.imwrite("img/forest/pic1_green.jpg", imgG)
-    cv2.imwrite("img/forest/pic1_red.jpg", imgR)
-
     images = []
     functions.set_window_size(200,200)
 
+    # reading
+    img = cv2.imread("img/forest/forest.jpeg", 1)
     images.append(["original", img])
+
+    # filtering
+    imgB = img[:,:,0]
     images.append(["blue", imgB])
+
+    imgG = img[:,:,1]
     images.append(["green", imgG])
+
+    imgR = img[:,:,2]
     images.append(["red", imgR])
 
+    functions.save(images, "jpeg", "forest")
     functions.show(images)
