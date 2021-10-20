92b669b7d99e206dca89a7072abbb085c70f37bc
Znypr <mulyadhic@gmail.com>
2021-10-20 21:13:49 +0200

    new functions: blur(), filter_custom()

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 import cv2
-import imghdr
+import numpy as np
 
 w, h = 600, 300
 
@@ -23,3 +23,18 @@ def save(images, type, folder):
     for i in range(1, len(images)):
         path = "img/" + folder +"/"+ folder + "_" + images[i][0] + "." + type
         cv2.imwrite(path, images[i][1])
+
+# FILTER
+
+def blur(img, x):
+    kernel = np.ones((x,x))/x**2
+
+    filtered = cv2.filter2D(img, -1, kernel)
+
+    return filtered
+
+def filter_custom(img, matrix):
+    kernel = np.array(matrix)
+
+    filtered = cv2.filter2D(img, -1, kernel)
+    return filtered
