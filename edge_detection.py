e4ac956210ddadc2802820c3a17dcdec4439a383
Znypr <mulyadhic@gmail.com>
2021-10-21 00:05:45 +0200

    Canny Edge Detection (functional)

--- edge_detection.py
+++ edge_detection.py
@@ -2,5 +2,23 @@ import cv2
 import functions
 import numpy as np
 
+
+
 if __name__ == '__main__':
 
+    images = []
+    img = cv2.imread("img/bicycle/bike.png", 0)
+    images.append(["original", img])
+
+    img1 = cv2.Canny(img, 0,255)
+    images.append(["canny", img1])
+
+    img2 = cv2.Canny(img, 0, 255, apertureSize=5)
+    images.append(["canny-aperture_5", img2])
+
+    img3 = cv2.Canny(img, 0, 255, L2gradient=True)
+    images.append(["canny-L2_true", img3])
+
+
+    functions.save(images, "png", "bicycle")
+    functions.show(images)
